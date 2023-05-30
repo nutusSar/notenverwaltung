ef3c2d66cac29da4ab5ca1379d2922c66c48c7ce
nutusSar <leion367@gmail.com>
2023-05-30 16:34:31 +0200

    Designed the Delete Gui

--- Notenverwaltung/srcJava/gui/GDelete.java
+++ Notenverwaltung/srcJava/gui/GDelete.java
@@ -6,6 +6,8 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 import dataclasses.Components;
 import logic.DeleteButton;
@@ -15,6 +17,9 @@ import logic.DeleteOk;
 public class GDelete {
 	
 	private JButton okButton; 
+	private JLabel label;
+	private JTextField textField;
+	private Font font = new Font("Arial", Font.PLAIN, 11);
 	
 	public GDelete(Components components, String cell) {
 		JFrame frame = new JFrame() {
@@ -28,10 +33,11 @@ public class GDelete {
 		};
 		frame.setBounds(300, 250, 400, 300);
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		frame.setLayout(null);
+		frame.getContentPane().setLayout(null);
+		frame.setTitle("Attention");
 		frame.setVisible(true);
 		okButton = new JButton("OK");
-		okButton.setFont(new Font("Arial", Font.PLAIN, 11));
+		okButton.setFont(font);
 		okButton.setBounds(325, 235, 50, 20);
 		okButton.addActionListener(new ActionListener() {
 				@Override
@@ -42,9 +48,20 @@ public class GDelete {
 				}
 			});
 		
-		frame.add(okButton);
+		frame.getContentPane().add(okButton);
+		
+		textField = new JTextField();
+		textField.setBounds(40, 147, 300, 20);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		String deleteMessage = "Warning! You are about to delete a " + components.getSubjectView().get("10").getText() + ". If you are sure, \nplease enter: \" IWantToDelete_" + components.getSubjectView().get(cell).getText() +"\"";
+		deleteMessage = "<html>" + deleteMessage.replaceAll("<","&lt;").replaceAll(">", "&gt;").replaceAll("\n", "<br/>").replace("\"", "<i>").replace("\"", "</i>") + "</html>";
+		label = new JLabel(deleteMessage);
+		label.setFont(font);
+		label.setBounds(40, 90, 300, 28);
+		frame.getContentPane().add(label);
 
+		
 	
 	}
-
 }
